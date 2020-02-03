02539f2be25ab264b3bf1fd72d0947de7dd344c4
Warayut Taekrathok <warayut2555@gmail.com>
2020-02-03 13:58:40 +0700

    back up

--- pages/index.js
+++ pages/index.js
@@ -1,159 +1,156 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
 import Head from '../components/head';
-import Nav from '../components/nav';
 
 const Home = () => {
-  const [date, setDate] = useState(null);
+	const [ date, setDate ] = useState(null);
+	const [ array, setArray ] = useState([
+		{ w: 1 },
+		{ w: 1 },
+		{ w: 1 },
+		{ w: 1 },
+		{ w: 1 },
+		{ w: 1 },
+		{ w: 1 },
+		{ w: 1 },
+		{ w: 1 },
+		{ w: 1 }
+	]);
 
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.json();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+	useEffect(() => {
+		async function getDate() {
+			const res = await fetch('/api/date');
+			const newDate = await res.json();
+			setDate(newDate);
+		}
+		getDate();
+	}, []);
 
-  return (
-    <div>
-      {/* <Head title="Home" />
-      <Nav /> */}
-      <link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet"></link>
+	return (
+		<div>
+			<Head title="Home" />
 
-      <div className="hero">
-        <center>
-          <img src="/logobrand66.png" width="500px"/>
-        </center>
-        <h1 className="title">สูตรบาคาร่า</h1>
-        {/* <p className="description">
-          To get started, edit the <code>pages/index.js</code> or{' '}
-          <code>pages/api/date.js</code> files, then save to reload.
-        </p> */}
+			<link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet" />
+			<div className="hero">
+				<center>
+					<img src="/logobrand66.png" width="500px" />
+				</center>
+				<h1 className="title">สูตรบาคาร่า</h1>
+				<center class="pt-5">
+					<div
+						class="container"
+						style={{
+							width: '610px'
+						}}
+					>
+						<div class="row justify-content-md-center w50">
+							{array ? (
+								array.map((element) => (
+									<div class="col-1 nopadding">
+										<img
+											src="/P.png"
+											width="50px"
+											alt="..."
+											class="rounded nopadding"
+										/>
+										<img
+											src="/P.png"
+											width="50px"
+											alt="..."
+											class="rounded nopadding"
+										/>
+										<img
+											src="/T.png"
+											width="50px"
+											alt="..."
+											class="rounded nopadding"
+										/>
+										<img
+											src="/P.png"
+											width="50px"
+											alt="..."
+											class="rounded nopadding"
+										/>
+										<img
+											src="/B.png"
+											width="50px"
+											alt="..."
+											class="rounded nopadding"
+										/>
+									</div>
+								))
+							) : (
+								<span className="loading" />
+							)}
+						</div>
+					</div>
+				</center>
+				<div
+					class="container nopadding p-3"
+					style={{
+						width: '610px'
+					}}
+				>
+					<div class="row nopadding justify-content-md-center">
+						<div class="col nopadding" style={{ paddingRight: '0px !important', paddingLeft: '0px !important'}}>
+							<button type="button" class="btn nopadding btn-primary btn-lg btn-block">
+								ผู้เล่น
+							</button>
+						</div>
+						<div class="col nopadding" style={{ paddingRight: '0px !important', paddingLeft: '0px !important'}}>
+							<button type="button" class="btn nopadding btn-success btn-lg btn-block">
+								เสมอ
+							</button>
+						</div>
+						<div class="col nopadding" style={{ paddingRight: '0px !important', paddingLeft: '0px !important'}}>
+							<button type="button" class="btn nopadding btn-danger btn-lg btn-block">
+								เข้ามือ
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 
-        {/* <p className="row date">
-          The date is:&nbsp;{' '}
-          {date ? (
-            <span>
-              <b>{date.date}</b>
-            </span>
-          ) : (
-            <span className="loading"></span>
-          )}
-        </p> */}
-
-
-{/* 
-
-        <div className="row">
-          <Link href="https://github.com/zeit/next.js#setup">
-            <a className="card">
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next.js on GitHub and in their examples.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js/tree/master/examples">
-            <a className="card">
-              <h3>Examples &rarr;</h3>
-              <p>Find other example boilerplates on the Next.js GitHub.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js">
-            <a className="card">
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it!</p>
-            </a>
-          </Link>
-        </div> */}
-{/*  */}
-
-
-
-{/*  */}
-
-      </div>
-
-      <style jsx>{`
-      *{
-        font-family: 'Prompt', sans-serif;
-      }
-        .hero {
-          width: 100%;
-          color: #333;
-        }
-        .title {
-          margin: 0;
-          width: 100%;
-          padding-top: 20px;
-          line-height: 1.15;
-          font-size: 48px;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .row {
-          max-width: 880px;
-          margin: 80px auto 40px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-around;
-        }
-        .date {
-          height: 24px;
-          max-width: calc(100% - 32px)
-          text-align: center;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          padding: 0 16px;
-        }
-        .date p {
-          text-align: center;
-        }
-        .date span {
-          width: 176px;
-          text-align: center;
-        }
-        @keyframes Loading {
-          0%{background-position:0% 50%}
-          50%{background-position:100% 50%}
-          100%{background-position:0% 50%}
-        }
-        .date .loading {
-          max-width: 100%;
-          height: 24px;
-          border-radius: 4px;
-          display: inline-block;
-          background: linear-gradient(270deg, #D1D1D1, #EAEAEA);
-          background-size: 200% 200%;
-          animation: Loading 2s ease infinite;
-        }
-        .card {
-          padding: 18px 18px 24px;
-          width: 220px;
-          text-align: left;
-          text-decoration: none;
-          color: #434343;
-          border: 1px solid #9b9b9b;
-        }
-        .card:hover {
-          border-color: #067df7;
-        }
-        .card h3 {
-          margin: 0;
-          color: #067df7;
-          font-size: 18px;
-        }
-        .card p {
-          margin: 0;
-          padding: 12px 0 0;
-          font-size: 13px;
-          color: #333;
-        }
-      `}</style>
-    </div>
-  );
+			<style jsx>{`
+				* {
+					font-family: 'Prompt', sans-serif;
+					background-color: #eeeeee;
+				}
+				body {
+					background-color: #eeeeee;
+					width: 100vw;
+					height: 100vh;
+				}
+				.nopadding {
+					padding: 0 !important;
+					margin: 0 !important;
+				}
+				.hero {
+					background-color: #eeeeee;
+					width: 100vw;
+					height: 100vh;
+					color: #333;
+				}
+				.title {
+					margin: 0;
+					width: 100%;
+					padding-top: 20px;
+					line-height: 1.15;
+					font-size: 48px;
+				}
+				.title,
+				.description {
+					text-align: center;
+				}
+				.row {
+					max-width: 880px;
+					margin: 80px auto 40px;
+					display: flex;
+					flex-direction: row;
+					justify-content: space-around;
+				}
+			`}</style>
+		</div>
+	);
 };
 
 export default Home;
